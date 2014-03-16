59a817a627d8166e1a74782b8ba4d3cedb9ad53c
lcrandol <levi_roble@yahoo.com>
2014-03-16 00:27:43 -0700

    all the files I have created for the project
    I have already made some changes to these files

---
+++ print.h
@@ -0,0 +1,17 @@
+//
+//  print.h
+//  Lab3
+//
+//  Created by Bryce Holton. Modified by Levi Randolph-Roble
+//  Copyright (c) 2014 Bryce Holton. All rights reserved.
+//
+
+#ifndef Lab3_print_h
+#define Lab3_print_h
+
+#include "common.h"
+
+void print_line(char line[], char source_name_to_print[], char date_to_print[]);
+void print_token(Token *token);
+
+#endif
